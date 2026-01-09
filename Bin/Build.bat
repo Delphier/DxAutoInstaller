b3f642366cdd01ee21e7b267b6db500b823d419b
Sid <netrobo@gmail.com>
2026-01-09 18:44:40 +0800

    Build.bat: Add platform identifier to the ProductVersion field in version information

--- Bin/Build.bat
+++ Bin/Build.bat
@@ -5,7 +5,7 @@ cls
 set AppName=DxAutoInstaller
 set /p Version=<..\Resources\Version.txt
 set Version=%Version:'=%
-set VersionInfo="CompanyName=https://github.com/Delphier/DxAutoInstaller;FileDescription=DevExpress VCL Components Automatic Installer;FileVersion=%Version%;LegalCopyright=Copyright © 2014-2026 faceker@gmail.com;ProductName=%AppName%;ProductVersion=%Version%"
+set VersionInfo=CompanyName=https://github.com/Delphier/DxAutoInstaller;FileDescription=DevExpress VCL Components Automatic Installer;FileVersion=%Version%;LegalCopyright=Copyright © 2014-2026 faceker@gmail.com;ProductName=%AppName%;ProductVersion=%Version%
 
 call "C:\Program Files (x86)\Embarcadero\Studio\37.0\bin\rsvars.bat"
 call :Build Win32 -x86
@@ -15,5 +15,5 @@ pause
 exit /b
 
 :Build
-	MSBuild "..\%AppName%.dproj" /t:Build /p:config=Release /p:platform=%1 /p:VerInfo_IncludeVerInfo=true /p:VerInfo_Keys=%VersionInfo%
+	MSBuild "..\%AppName%.dproj" /t:Build /p:config=Release /p:platform=%1 /p:VerInfo_IncludeVerInfo=true /p:VerInfo_Keys="%VersionInfo%_%~1"
 	tar -caf %AppName%-%Version%%~2.zip %AppName%.exe
