0a35201099f7492102488284c44179e83ed16f7c
Sid <netrobo@gmail.com>
2026-02-03 22:32:43 +0800

    Refactor: Create TPlatform, TIDE

--- Sources/DxAutoInstaller.Core.pas
+++ Sources/DxAutoInstaller.Core.pas
@@ -1,6 +1,20 @@
+{*******************************************************}
+{                                                       }
+{             DxAutoInstaller Core Library              }
+{                                                       }
+{      https://github.com/Delphier/DxAutoInstaller      }
+{                                                       }
+{      Copyright(c) 2014-2026 faceker@gmail.com         }
+{              All rights reserved                      }
+{                                                       }
+{*******************************************************}
+
 unit DxAutoInstaller.Core;
 
 interface
+
+uses
+  JclIDEUtils, JclCompilerUtils;
 
 type
   TApp = class
@@ -8,6 +22,87 @@ type
     Version = {$I ..\Resources\Version.txt};
   end;
 
+  TJclIDE = TJclBDSInstallation;
+  TJclPlatform = TJclBDSPlatform;
+  TJclCompiler = TJclBorlandCommandLineTool;
+
+  TPlatform = (pfWin32, pfWin64, pfWin64x);
+  TPlatforms = set of TPlatform;
+
+  TIDE = class
+  private
+    FIDE: TJclIDE;
+    FPackageVersionStr: string;
+    FDelphiInstalled: Boolean;
+    FCppBuilderInstalled: Boolean;
+    FSupportedPlatforms: TPlatforms;
+  public
+    constructor Create(AIDE: TJclIDE);
+    property PackageVersionStr: string read FPackageVersionStr;
+    property DelphiInstalled: Boolean read FDelphiInstalled;
+    property CppBuilderInstalled: Boolean read FCppBuilderInstalled;
+    property SupportedPlatforms: TPlatforms read FSupportedPlatforms;
+  end;
+
+  TCompiler = TJclCompiler;
+  TCompilerGetter = function(AIDE: TIDE): TCompiler;
+
+  TPlatformHelper = record helper for TPlatform
+  private
+    class function Win32Compiler(AIDE: TIDE): TCompiler; static;
+    class function Win64Compiler(AIDE: TIDE): TCompiler; static;
+  public
+    function ToJclValue: TJclPlatform;
+    function Compiler(AIDE: TIDE): TCompiler;
+  end;
+
+const
+  PlatformJclValues          : array[TPlatform] of TJclPlatform      = (bpWin32, bpWin64, bpWin64x);
+  PlatformNames              : array[TPlatform] of string            = ('Win32', 'Win64', 'Win64x');
+  PlatformDescriptions       : array[TPlatform] of string            = ('Windows 32-bit', 'Windows 64-bit', 'Windows 64-bit (Modern)');
+  PlatformCompilers          : array[TPlatform] of TCompilerGetter   = (TPlatform.Win32Compiler, TPlatform.Win64Compiler, TPlatform.Win64Compiler);
+  PlatformCommandLineTools   : array[TPlatform] of TCommandLineTool  = (clDcc32, clDcc64, clBcc64x);
+  DelphiPlatforms            = [pfWin32, pfWin64];
+  DelphiPlatformsDefault     = [pfWin32];
+  CppBuilderPlatforms        = [pfWin32, pfWin64, pfWin64x];
+  CppBuilderPlatformsDefault = [pfWin64x];
+
 implementation
+
+uses
+  System.SysUtils;
+
+{ TIDE }
+
+constructor TIDE.Create(AIDE: TJclIDE);
+begin
+  FIDE := AIDE;
+  FPackageVersionStr := FIDE.VersionNumberStr.Replace('d', 'RS');
+  FDelphiInstalled := FIDE.Personalities * [bpDelphi32, bpDelphi64] <> [];
+  FCppBuilderInstalled := FIDE.Personalities * [bpBCBuilder32, bpBCBuilder64] <> [];
+  for var I := Low(TPlatform) to High(TPlatform) do if PlatformCommandLineTools[I] in FIDE.CommandLineTools then Include(FSupportedPlatforms, I);
+end;
+
+{ TPlatformHelper }
+
+function TPlatformHelper.ToJclValue: TJclPlatform;
+begin
+  Result := PlatformJclValues[Self];
+end;
+
+function TPlatformHelper.Compiler(AIDE: TIDE): TCompiler;
+begin
+  Result := PlatformCompilers[Self](AIDE);
+end;
+
+class function TPlatformHelper.Win32Compiler(AIDE: TIDE): TCompiler;
+begin
+  Result := AIDE.FIDE.DCC32;
+end;
+
+class function TPlatformHelper.Win64Compiler(AIDE: TIDE): TCompiler;
+begin
+  Result := AIDE.FIDE.DCC64;
+end;
 
 end.
