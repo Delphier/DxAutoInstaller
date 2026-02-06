b8e0ece3643bdfbd62687ab3ad6e505b406ace84
Sid <netrobo@gmail.com>
2026-02-06 17:41:23 +0800

    Refactor: Create TPackage, TPackageName

--- Sources/DxAutoInstaller.DevExpress.pas
+++ Sources/DxAutoInstaller.DevExpress.pas
@@ -13,37 +13,56 @@ unit DxAutoInstaller.DevExpress;
 
 interface
 
-type
-  TManifest = class
-  type
-    TComponent = record
-      Name: string;
-      IsBase: Boolean;
-      RequiredPackages: TArray<string>;
-      OptionalPackages: TArray<string>;
-      OutdatedPackages: TArray<string>;
-    end;
+uses
+  DxAutoInstaller.Core;
 
+type
+  TComponentMetadata = record
+    Name: string;
+    IsBase: Boolean;
+    RequiredPackages: TArray<string>;
+    OptionalPackages: TArray<string>;
+    OutdatedPackages: TArray<string>; // Uninstall only.
+  end;
+
+  TManifest = class
   const
     CustomFileBaseName  = 'DevExpress.ini';
     ResourceName        = 'DevExpressManifest';
   private
     FIsCustom: Boolean;
-    FComponents: TArray<TComponent>;
+    FComponents: TArray<TComponentMetadata>;
     procedure ReadComponents;
   public
     class function CustomFileName: string;
     class function CustomFileExists: Boolean;
     constructor Create(const AUseCustomFile: Boolean);
     property IsCustom: Boolean read FIsCustom;
-    property Components: TArray<TComponent> read FComponents;
+    property Components: TArray<TComponentMetadata> read FComponents;
     procedure Export;
+  end;
+
+  TPackageName = type string;
+  TPackageNameHelper = record helper for TPackageName
+    constructor Create(const APackageBaseName: string; AIDE: TIDE);
+    function IsDesigntime: Boolean;
+  end;
+
+  TPackage = record
+    Name: TPackageName;
+    FileName: string;
+    Description: string;
+    Requires: TArray<string>;
+    class procedure ParseFile(const AFileName: string; out ADescription: string; out ARequires: TArray<string>); static;
+    constructor Create(const AName: TPackageName; const AFileName: string);
+    function Exists: Boolean;
   end;
 
 implementation
 
 uses
-  System.Classes, System.SysUtils, System.IOUtils, System.IniFiles, Vcl.Forms, DxAutoInstaller.Utils;
+  System.Classes, System.SysUtils, System.IOUtils, System.IniFiles, System.RegularExpressions,
+  Vcl.Forms, DxAutoInstaller.Utils;
 
 { TManifest }
 
@@ -83,7 +102,7 @@ begin
     try
       IniFile.ReadSections(Names);
       for var Name in Names do begin
-        var Component := Default(TComponent);
+        var Component := Default(TComponentMetadata);
         Component.Name := Name;
         Component.IsBase := IniFile.ReadBool(Name, 'IsBase', False);
         Component.RequiredPackages := IniFile.ReadString(Name, 'RequiredPackages', '').Split(Separators, TStringSplitOptions.ExcludeEmpty);
@@ -102,6 +121,57 @@ end;
 procedure TManifest.Export;
 begin
   ExportResourceToFile(CustomFileName, ResourceName);
+end;
+
+{ TPackage }
+
+constructor TPackage.Create(const AName: TPackageName; const AFileName: string);
+begin
+  Name := AName;
+  FileName := AFileName;
+  ParseFile(AFileName, Description, Requires);
+end;
+
+function TPackage.Exists: Boolean;
+begin
+  Result := TFile.Exists(FileName);
+end;
+
+class procedure TPackage.ParseFile(const AFileName: string; out ADescription: string; out ARequires: TArray<string>);
+begin
+  ADescription := '';
+  ARequires := [];
+
+  var Requires := '';
+  var InRequires := False;
+  for var Line in TFile.GetLinesEnumerator(AFileName) do begin
+    if InRequires then begin
+      var I := Line.IndexOf(';');
+      if I >= 0 then begin
+        Requires := Requires + Line.Remove(I);
+        ARequires := Requires.Split([',', ' '], TStringSplitOptions.ExcludeEmpty);
+        Break;
+      end else
+        Requires := Requires + Line;
+    end else if SameText(Line.Trim, 'requires') then
+      InRequires := True
+    else begin
+      var Match := TRegEx.Match(Line, '{\$DESCRIPTION ''(.*)''}'); // {$DESCRIPTION 'ExpressCharts by Developer Express Inc.'}
+      if Match.Success then ADescription := Match.Groups[1].Value;
+    end;
+  end;
+end;
+
+{ TPackageNameHelper }
+
+constructor TPackageNameHelper.Create(const APackageBaseName: string; AIDE: TIDE);
+begin
+  Self := APackageBaseName + AIDE.PackageVersionStr;
+end;
+
+function TPackageNameHelper.IsDesigntime: Boolean;
+begin
+  Result := string.StartsText('dcl', Self);
 end;
 
 end.
