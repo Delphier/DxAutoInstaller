a43026553d09fa3e16aa547f5b6740645612286a
Sid <netrobo@gmail.com>
2026-03-01 12:36:42 +0800

    Add TUserEnvironmentVariablePath to manage Path items

--- Sources/DxAutoInstaller.Utils.pas
+++ Sources/DxAutoInstaller.Utils.pas
@@ -14,11 +14,27 @@ unit DxAutoInstaller.Utils;
 interface
 
 uses
-  System.Classes, Winapi.Windows;
+  System.Classes, System.Win.Registry;
 
 type
   TMessageBox = record
     class function Confirm(const AMessage: string; AButtonCaptions: TArray<string> = []): Boolean; static;
+  end;
+
+  TUserEnvironmentVariablePath = record
+  const
+    ENVIRONMENT = 'Environment';
+    PATH        = 'Path';
+  private
+    class var FRegistry: TRegistry;
+    class destructor Destroy;
+    class function Registry: TRegistry; static;
+    class function Read: TArray<string>; static;
+    class procedure Write(const AValue: TArray<string>); static;
+    class function SameItem(const A, B: string): Boolean; static;
+  public
+    class procedure Add(const AItem: string); static;
+    class procedure Remove(const AItem: string); static;
   end;
 
   TLoadFromStreamProc = procedure(AStream: TStream) of object;
@@ -30,7 +46,7 @@ procedure ExportResourceToFile(const AFileName, AResName: string);
 implementation
 
 uses
-  System.UITypes, Vcl.Dialogs;
+  System.SysUtils, System.UITypes, Winapi.Windows, Winapi.Messages, Vcl.Dialogs;
 
 { TMessageBox }
 
@@ -38,6 +54,54 @@ class function TMessageBox.Confirm(const AMessage: string; AButtonCaptions: TArr
 begin
   MsgDlgIcons[mtConfirmation] := TMsgDlgIcon.mdiInformation;
   Result := MessageDlg(AMessage, mtConfirmation, mbYesNo, 0, mbNo, AButtonCaptions) = mrYes;
+end;
+
+{ TUserEnvironmentVariablePath }
+
+class function TUserEnvironmentVariablePath.Registry: TRegistry;
+begin
+  if not Assigned(FRegistry) then FRegistry := TRegistry.Create;
+  Result := FRegistry;
+end;
+
+class destructor TUserEnvironmentVariablePath.Destroy;
+begin
+  FRegistry.Free;
+end;
+
+class function TUserEnvironmentVariablePath.Read: TArray<string>;
+begin
+  Registry.OpenKeyReadOnly(ENVIRONMENT);
+  Result := Registry.ReadString(PATH).Split([';']);
+  Registry.CloseKey;
+end;
+
+class procedure TUserEnvironmentVariablePath.Write(const AValue: TArray<string>);
+begin
+  Registry.OpenKey(ENVIRONMENT, True);
+  Registry.WriteString(PATH, string.Join(';', AValue));
+  Registry.CloseKey;
+  SendMessage(HWND_BROADCAST, WM_SETTINGCHANGE, 0, LPARAM(PChar(ENVIRONMENT)));
+end;
+
+class function TUserEnvironmentVariablePath.SameItem(const A, B: string): Boolean;
+begin
+  Result := SameFileName(ExcludeTrailingPathDelimiter(A.Trim), ExcludeTrailingPathDelimiter(B.Trim));
+end;
+
+class procedure TUserEnvironmentVariablePath.Add(const AItem: string);
+begin
+  var List := Read;
+  for var Item in List do if SameItem(Item, AItem) then Exit;
+  Write(List + [AItem]);
+end;
+
+class procedure TUserEnvironmentVariablePath.Remove(const AItem: string);
+begin
+  var List := Read;
+  var Count := Length(List);
+  for var I := High(List) downto Low(List) do if SameItem(List[I], AItem) then Delete(List, I, 1);
+  if Count <> Length(List) then Write(List);
 end;
 
 function CreateResourceStream(const AResName: string): TResourceStream;
